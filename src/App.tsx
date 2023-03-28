e44502ff6b425946a31a5c3ffedeaa58340c3c2f
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2023-03-28 19:57:12 -0300

    feat: projects

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { About } from './components/about'
 import { Experiences } from './components/Experiences'
 import { Header } from './components/Header'
 import { Home } from './components/Home'
+import { Projects } from './components/projects'
 import './global/styles.scss'
 import './sass/_index.scss'
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Experiences />
+      <Projects />
     </>
   )
 }
---
+++ src/components/projects/index.tsx
@@ -0,0 +1,71 @@
+import github from '../../assets/images/about/github.svg'
+
+import './index.scss'
+
+export function Projects() {
+  return (
+    <main className='projects'>
+      <div className="container-center">
+        <h3>PROJETOS</h3>
+        <section>
+          <nav className='project'>
+            <img src="https://github.com/jonathas-bonfim.png" alt="Project" className='image-project' />
+            <h4>Loja Speedo</h4>
+            <p>Projeto e-commerce em Vtex IO de moda masculina e feminina</p>
+            <h5>Tecnologias usadas no projeto</h5>
+            <div className='icons-project'>
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+            </div>
+            <a href="https://www.speedo.com.br/" target="_blank">Visualizar</a>
+          </nav>
+          <nav className='project'>
+            <img src="https://github.com/jonathas-bonfim.png" alt="Project" className='image-project' />
+            <h4>Loja Speedo</h4>
+            <p>Projeto e-commerce em Vtex IO de moda masculina e feminina</p>
+            <h5>Tecnologias usadas no projeto</h5>
+            <div className='icons-project'>
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+            </div>
+            <a href="https://www.speedo.com.br/" target="_blank">Visualizar</a>
+          </nav>
+          <nav className='project'>
+            <img src="https://github.com/jonathas-bonfim.png" alt="Project" className='image-project' />
+            <h4>Loja Speedo</h4>
+            <p>Projeto e-commerce em Vtex IO de moda masculina e feminina</p>
+            <h5>Tecnologias usadas no projeto</h5>
+            <div className='icons-project'>
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+            </div>
+            <a href="https://www.speedo.com.br/" target="_blank">Visualizar</a>
+          </nav>
+          <nav className='project'>
+            <img src="https://github.com/jonathas-bonfim.png" alt="Project" className='image-project' />
+            <h4>Loja Speedo</h4>
+            <p>Projeto e-commerce em Vtex IO de moda masculina e feminina</p>
+            <h5>Tecnologias usadas no projeto</h5>
+            <div className='icons-project'>
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+              <img src={github} />
+            </div>
+            <a href="https://www.speedo.com.br/" target="_blank">Visualizar</a>
+          </nav>
+        </section>
+      </div>
+    </main>
+  )
+}
