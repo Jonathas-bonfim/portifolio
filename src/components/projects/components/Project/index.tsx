561616f191a4a3b59508007dce76c2f9ceed3a5b
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2023-03-29 11:03:22 -0300

    feat: project

---
+++ src/components/projects/components/Project/index.tsx
@@ -0,0 +1,30 @@
+import github from '../../../../assets/images/about/github.svg'
+
+type ProjectProps = {
+  name: string,
+  description: string,
+  icon: string
+  icon2: string
+  icon3?: string
+  icon4?: string
+  link: string,
+  image: string
+}
+
+export function Project({ name, description, link, image, icon, icon2, icon3, icon4 }: ProjectProps) {
+  return (
+    <nav className='project'>
+      <img src={image} alt="Project" className='image-project' />
+      <h4>{name}</h4>
+      <p>{description}</p>
+      <h5>Tecnologias usadas no projeto</h5>
+      <div className='icons-project'>
+        <img src={icon} />
+        <img src={icon2} />
+        <img src={icon3} />
+        <img src={icon4} />
+      </div>
+      <a href={link} target="_blank">Visualizar</a>
+    </nav>
+  )
+}
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,5 +1,4 @@
-import github from '../../assets/images/about/github.svg'
-
+import { Project } from './components/Project'
 import './index.scss'
 
 export function Projects() {
@@ -8,62 +7,47 @@ export function Projects() {
       <div className="container-center">
         <h3>PROJETOS</h3>
         <section>
-          <nav className='project'>
-            <img src="https://github.com/jonathas-bonfim.png" alt="Project" className='image-project' />
-            <h4>Loja Speedo</h4>
-            <p>Projeto e-commerce em Vtex IO de moda masculina e feminina</p>
-            <h5>Tecnologias usadas no projeto</h5>
-            <div className='icons-project'>
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-            </div>
-            <a href="https://www.speedo.com.br/" target="_blank">Visualizar</a>
-          </nav>
-          <nav className='project'>
-            <img src="https://github.com/jonathas-bonfim.png" alt="Project" className='image-project' />
-            <h4>Loja Speedo</h4>
-            <p>Projeto e-commerce em Vtex IO de moda masculina e feminina</p>
-            <h5>Tecnologias usadas no projeto</h5>
-            <div className='icons-project'>
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-            </div>
-            <a href="https://www.speedo.com.br/" target="_blank">Visualizar</a>
-          </nav>
-          <nav className='project'>
-            <img src="https://github.com/jonathas-bonfim.png" alt="Project" className='image-project' />
-            <h4>Loja Speedo</h4>
-            <p>Projeto e-commerce em Vtex IO de moda masculina e feminina</p>
-            <h5>Tecnologias usadas no projeto</h5>
-            <div className='icons-project'>
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-            </div>
-            <a href="https://www.speedo.com.br/" target="_blank">Visualizar</a>
-          </nav>
-          <nav className='project'>
-            <img src="https://github.com/jonathas-bonfim.png" alt="Project" className='image-project' />
-            <h4>Loja Speedo</h4>
-            <p>Projeto e-commerce em Vtex IO de moda masculina e feminina</p>
-            <h5>Tecnologias usadas no projeto</h5>
-            <div className='icons-project'>
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-              <img src={github} />
-            </div>
-            <a href="https://www.speedo.com.br/" target="_blank">Visualizar</a>
-          </nav>
+          <Project
+            name='Loja Speedo'
+            description='Projeto e-commerce em Vtex IO de equipamentos esportivos.'
+            icon='https://img.shields.io/badge/-React-05122A?style=flat&logo=react'
+            icon2='https://img.shields.io/badge/-TypeScript-05122A?style=flat&logo=typescript'
+            icon3='https://img.shields.io/badge/-Sass-05122A?style=flat&logo=sass'
+            icon4='https://img.shields.io/badge/-vtex-05122A?style=flat&logo=vtex'
+            image='https://firebasestorage.googleapis.com/v0/b/letmeask-87b25.appspot.com/o/speedo.png?alt=media&token=3baf6c43-2370-4a9e-9dcd-e59b37656e5a'
+            link='https://www.speedo.com.br/'
+          />
+          <Project
+            name='Jean Darrot'
+            description='Projeto e-commerce em Vtex IO de moda masculina e feminina.'
+            icon='https://img.shields.io/badge/-React-05122A?style=flat&logo=react'
+            icon2='https://img.shields.io/badge/-TypeScript-05122A?style=flat&logo=typescript'
+            icon3='https://img.shields.io/badge/-Sass-05122A?style=flat&logo=sass'
+            icon4='https://img.shields.io/badge/-vtex-05122A?style=flat&logo=vtex'
+            image='https://firebasestorage.googleapis.com/v0/b/letmeask-87b25.appspot.com/o/jeandarrot.png?alt=media&token=81b7562a-8093-4653-897c-fc4ad9bc46e7'
+            link='https://www.jeandarrot.com.br/'
+          />
+          <Project
+            name='Estech'
+            description='Projeto e-commerce em Vtex IO de Aparelhos eletrônicos.'
+            icon='https://img.shields.io/badge/-React-05122A?style=flat&logo=react'
+            icon2='https://img.shields.io/badge/-TypeScript-05122A?style=flat&logo=typescript'
+            icon3='https://img.shields.io/badge/-Sass-05122A?style=flat&logo=sass'
+            icon4='https://img.shields.io/badge/-vtex-05122A?style=flat&logo=vtex'
+            image='https://firebasestorage.googleapis.com/v0/b/letmeask-87b25.appspot.com/o/estech.png?alt=media&token=c90fb308-0be5-4a47-b96f-9da893178fa4'
+            link='https://youtu.be/eJjdyKgVYE0?list=PL54q87_dQSc5ErWIj5NVhbIoYba_jcAWJ'
+          />
+
+          <Project
+            name='Atual Social'
+            description='Projeto corporativo interno para funcionários da empresa.'
+            icon='https://img.shields.io/badge/-React-05122A?style=flat&logo=react'
+            icon2='https://img.shields.io/badge/-TypeScript-05122A?style=flat&logo=typescript'
+            icon3='https://img.shields.io/badge/-Sass-05122A?style=flat&logo=sass'
+            icon4='https://img.shields.io/badge/-vtex-05122A?style=flat&logo=vtex'
+            image='https://firebasestorage.googleapis.com/v0/b/letmeask-87b25.appspot.com/o/atualsocial.png?alt=media&token=5b33bded-413f-4787-9baf-33be37bdf729'
+            link='https://youtu.be/vG9A6kNlfdE?list=PL54q87_dQSc5ErWIj5NVhbIoYba_jcAWJ'
+          />
         </section>
       </div>
     </main>
